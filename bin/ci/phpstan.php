cb121bba68a912df2ed27775d145e6d4b6cab3fb
Steevan BARBOYON <steevan.barboyon@gmail.com>
2023-07-05 20:11:00 +0200

    Add Symfony 6.3 support, update ci dependencies

--- bin/ci/phpstan.php
+++ bin/ci/phpstan.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use Steevanb\ParallelProcess\{
     Console\Application\ParallelProcessesApplication,
     Process\Process,
-    Process\ProcessArray
+    Process\ProcessInterfaceCollection
 };
 use Steevanb\PhpCollection\ScalarCollection\StringCollection;
 use Symfony\Component\Console\Input\ArgvInput;
@@ -13,13 +13,13 @@ use Symfony\Component\Console\Input\ArgvInput;
 require $_SERVER['COMPOSER_HOME'] . '/vendor/autoload.php';
 require dirname(__DIR__, 2) . '/vendor/autoload.php';
 
-function createPhpstanProcesses(): ProcessArray
+function createPhpstanProcesses(): ProcessInterfaceCollection
 {
     $phpVersions = new StringCollection(['8.1', '8.2']);
 
-    $return = new ProcessArray();
+    $return = new ProcessInterfaceCollection();
     foreach ($phpVersions->toArray() as $loopPhpVersion) {
-        $return[] = createPhpstanProcess($loopPhpVersion);
+        $return->add(createPhpstanProcess($loopPhpVersion));
     }
 
     return $return;
